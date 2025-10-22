a1061754ccc2a60379c867d5dc0d865cd9c747b0
HKRph <jethcarloph123@gmail.com>
2025-10-22 21:02:05 +0800

    Fix indentation in main.py

--- main.py
+++ main.py
@@ -155,4 +155,4 @@ ptb_app.add_handler(CallbackQueryHandler(remove_task_list, pattern="^remove_task
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)```
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
